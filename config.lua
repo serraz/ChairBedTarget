13a8ab142f3168665a3f45c64eedcd42a799f380
Serraznet Gaming <c_serrao@hotmail.com>
2022-01-21 17:33:04 +1100

    Fixed spacing a bit cause it was anoying my OCD

--- config.lua
+++ config.lua
@@ -9,8 +9,8 @@ Config.objects = {
 	BedSitAnimation = {anim='WORLD_HUMAN_PICNIC'},
 	locations = {
 		[2117668672] = {object=2117668672, verticalOffsetX=0.0, verticalOffsetY=0.0, verticalOffsetZ=-1.4, direction=0.0, bed=true},
-        [1631638868] = {object=1631638868, verticalOffsetX=0.0, verticalOffsetY=0.0, verticalOffsetZ=-1.4, direction=0.0, bed=true},
-        [-1519439119] = {object=-1519439119, verticalOffsetX=0.0, verticalOffsetY=0.0, verticalOffsetZ=-2.0, direction=0.0, bed=true},
+		[1631638868] = {object=1631638868, verticalOffsetX=0.0, verticalOffsetY=0.0, verticalOffsetZ=-1.4, direction=0.0, bed=true},
+		[-1519439119] = {object=-1519439119, verticalOffsetX=0.0, verticalOffsetY=0.0, verticalOffsetZ=-2.0, direction=0.0, bed=true},
 		[-171943901] = {object=-171943901, verticalOffsetX=0.0, verticalOffsetY=0.0, verticalOffsetZ=-0.0, direction=168.0, bed=false},
 		[1268458364] = {object=1268458364, verticalOffsetX=0.0, verticalOffsetY=0.0, verticalOffsetZ=-0.4, direction=168.0, bed=false},
 		[96868307] = {object=96868307, verticalOffsetX=0.0, verticalOffsetY=0.0, verticalOffsetZ=-0.4, direction=168.0, bed=false},
@@ -20,15 +20,15 @@ Config.objects = {
 		[-109356459] = {object=-109356459, verticalOffsetX=0.0, verticalOffsetY=0.0, verticalOffsetZ=-0.4, direction=168.0, bed=false},
 		[-1091386327] = {object=-1091386327, verticalOffsetX=0.0, verticalOffsetY=0.13, verticalOffsetZ=-0.2, direction=90.0, bed=false},
 		[536071214] = {object=536071214, verticalOffsetX=0.0, verticalOffsetY=-0.1, verticalOffsetZ=-0.5, direction=180.0, bed=false},
-        [538002882] = {object=538002882, verticalOffsetX=0.0, verticalOffsetY=0.0, verticalOffsetZ=0.1, direction=168.0, bed=false},
-        [-1118419705] = {object=-1118419705, verticalOffsetX=0.0, verticalOffsetY=-0.1, verticalOffsetZ=-0.5, direction=168.0, bed=false},
-        [-992710074] = {object=-992710074, verticalOffsetX=0.0, verticalOffsetY=-0.1, verticalOffsetZ=-0.7, direction=168.0, bed=false},
-        [-1195678770] = {object=-1195678770, verticalOffsetX=0.0, verticalOffsetY=-0.1, verticalOffsetZ=-0.7, direction=5.0, bed=false},
-        [-992735415] = {object=-992735415, verticalOffsetX=0.0, verticalOffsetY=-0.0, verticalOffsetZ=0.1, direction=180.0, bed=false},
-        [-1761659350] = {object=-1761659350, verticalOffsetX=0.0, verticalOffsetY=-0.0, verticalOffsetZ=-0.5, direction=180.0, bed=false},
-        [-1626066319] = {object=-1626066319, verticalOffsetX=0.0, verticalOffsetY=-0.0, verticalOffsetZ=0.1, direction=180.0, bed=false}
-	}
-}
+		[538002882] = {object=538002882, verticalOffsetX=0.0, verticalOffsetY=0.0, verticalOffsetZ=0.1, direction=168.0, bed=false},
+		[-1118419705] = {object=-1118419705, verticalOffsetX=0.0, verticalOffsetY=-0.1, verticalOffsetZ=-0.5, direction=168.0, bed=false},
+		[-992710074] = {object=-992710074, verticalOffsetX=0.0, verticalOffsetY=-0.1, verticalOffsetZ=-0.7, direction=168.0, bed=false},
+		[-1195678770] = {object=-1195678770, verticalOffsetX=0.0, verticalOffsetY=-0.1, verticalOffsetZ=-0.7, direction=5.0, bed=false},
+		[-992735415] = {object=-992735415, verticalOffsetX=0.0, verticalOffsetY=-0.0, verticalOffsetZ=0.1, direction=180.0, bed=false},
+		[-1761659350] = {object=-1761659350, verticalOffsetX=0.0, verticalOffsetY=-0.0, verticalOffsetZ=-0.5, direction=180.0, bed=false},
+		[-1626066319] = {object=-1626066319, verticalOffsetX=0.0, verticalOffsetY=-0.0, verticalOffsetZ=0.1, direction=180.0, bed=false}
+		   }
+                 }
 
 Config.seats = {
 	538002882, -- MRPD Chairs 
@@ -36,8 +36,8 @@ Config.seats = {
 	-992710074, -- Pillbox Stools
 	-109356459, -- Chairs in Pillbox
 	-992735415, -- Chair in Sandy Customs Office
-    -1761659350, -- Vineyard Wood Chair
-    -1626066319, -- Stumpys Office Chair
+	-1761659350, -- Vineyard Wood Chair
+	-1626066319, -- Stumpys Office Chair
 	-171943901, 
 	1268458364, 
 	96868307, 
@@ -52,5 +52,5 @@ Config.seats = {
 Config.beds = {
 	1631638868, -- Hospital Beds 
 	2117668672, -- Hospital Beds 
-    -1519439119, -- MRI Machine Pillbox
+	-1519439119, -- MRI Machine Pillbox
 }
